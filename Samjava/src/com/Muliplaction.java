b1a7abd50e8a072c0ebe5b7b4abe8a2e1cbb661c
SYED <SYED@SYED-ATEEQUE-QADRI>
2022-06-03 13:15:36 +0530

    first commit

---
+++ Samjava/src/com/Muliplaction.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Muliplaction {
+
+	public static void main(String[] args) {
+	int a = 25;
+	int b = 5;
+	int c;
+		c = a * b;
+		System.out.println("The multiplaction of two numbers is = "+c);
+				
+				
+
+	}
+
+}
