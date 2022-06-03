189a3bc08af570619849f201263133484696357d
SYED <SYED@SYED-ATEEQUE-QADRI>
2022-06-03 13:07:20 +0530

    first commit

---
+++ Samjava/src/com/SUBSTRACTION.java
@@ -0,0 +1,14 @@
+package com;
+
+public class SUBSTRACTION {
+
+	public static void main(String[] args) {
+	int a= 10;
+	int b=5;
+	int c;
+	
+		c = a - b;
+		System.out.println("The Result is "+c);
+	}
+
+}
