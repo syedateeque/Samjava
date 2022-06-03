c766f69b31d5e8c520e525e82e2f3dfd0a96a53d
SYED <SYED@SYED-ATEEQUE-QADRI>
2022-06-03 12:53:29 +0530

    first commit

---
+++ Samjava/src/com/Addition.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Addition {
+
+		public static void main(String[] args)
+		{
+			int a = 5;
+			int b = 9;
+			int c;
+			c= a+b;
+			System.out.println("The result = " +c);
+
+		}
+
+	}
