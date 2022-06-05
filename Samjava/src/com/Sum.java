ad9e7ac9df0b1304217a06de03bef83f96a92ead
SYED <SYED@SYED-ATEEQUE-QADRI>
2022-06-05 17:45:49 +0530

    first commit

---
+++ Samjava/src/com/Sum.java
@@ -0,0 +1,20 @@
+package com;
+
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+	int c;
+	Scanner cs1 = new Scanner(System.in);
+	Scanner cs2 = new Scanner(System.in);
+	System.out.println("First number is = ");
+	int a = cs1.nextInt();
+	System.out.println("Second number is = ");
+	int b = cs2.nextInt();
+		c = a - b;
+		System.out.println("The anser is = "+c);
+
+	}
+
+}
