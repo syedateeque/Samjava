5839febb298681412ed8ff6f2f59c2d9412cdf20
SYED <SYED@SYED-ATEEQUE-QADRI>
2022-06-05 17:53:39 +0530

    first commit

---
+++ Samjava/src/com/Mul.java
@@ -0,0 +1,21 @@
+package com;
+
+import java.util.Scanner;
+
+public class Mul {
+
+	public static void main(String[] args) {
+	int c;
+	Scanner sam1 = new Scanner(System.in);
+	Scanner sam2 = new Scanner(System.in);
+	System.out.println("Enter the number ");
+	int a = sam1.nextInt();
+	System.out.println("Enter the number ");
+	int b = sam2.nextInt();
+	c = a * b ;
+	System.out.println("Multiplaction of two numbers is = "+c);
+
+
+	}
+
+}
