db5d0ca71f5722001f0655dcf4c5f7eadaafd2d5
SYED <SYED@SYED-ATEEQUE-QADRI>
2022-06-05 16:54:41 +0530

    first commit

---
+++ Samjava/src/com/Division.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Division {
+
+	public static void main(String[] args) {
+	int a= 725;
+	int b= 25;
+	int c;
+		c = a / b;
+		System.out.println("The division of given question is = "+c);
+
+	}
+
+}
