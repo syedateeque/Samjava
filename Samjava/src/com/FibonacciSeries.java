696b4cb3d147171ac78acfcb3fb11e0b066dfc2a
SYED <SYED@SYED-ATEEQUE-QADRI>
2022-06-06 22:07:27 +0530

    second commit

---
+++ Samjava/src/com/FibonacciSeries.java
@@ -0,0 +1,21 @@
+package com;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args)
+	{
+		int a=0, b=1 ;
+		System.out.println(a+""+b);
+		int c;
+		for (int i=1;i<=100;i++);
+		{
+			c=a+b;
+			System.out.println(""+c);
+			a=b;
+			b=c;
+		}
+		
+
+	}
+
+}
