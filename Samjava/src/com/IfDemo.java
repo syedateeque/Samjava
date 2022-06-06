8c5c0837bf0c5675992f512fbe36777e0b0cfef9
SYED <SYED@SYED-ATEEQUE-QADRI>
2022-06-06 22:24:50 +0530

    second commit

---
+++ Samjava/src/com/IfDemo.java
@@ -0,0 +1,19 @@
+package com;
+
+public class IfDemo {
+	static void withdraw()
+	{
+		System.out.println("Money is Withdraw");
+	}
+
+	public static void main(String[] args) {
+		int balance = 100;
+		if (balance>500)
+			IfDemo.withdraw();
+		System.out.println("Simple if demonstration");
+	
+		
+	}
+	
+
+}
