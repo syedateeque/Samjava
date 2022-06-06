1c8b6042dc53ae5cead9f8a3bec73fd87cc733a4
SYED <SYED@SYED-ATEEQUE-QADRI>
2022-06-06 22:14:06 +0530

    second commit

---
+++ Samjava/src/com/Foreachloop.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Foreachloop {
+
+	public static void main(String[] args)
+	{
+	  int s [] = {5,10,23,44,43,67,11};
+	  for (int i : s)
+	  {
+		  System.out.println(i);
+	  }
+	
+
+	}
+
+}
